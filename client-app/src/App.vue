2c2950c70ec31251c5486209ffdd5f38e2ef71c9
Un1c0re <sayapin.p.a02@gmail.com>
2024-11-02 22:45:12 +0500

    [:sparkles:] Добавлен просмотр pdf документа

--- client-app/src/App.vue
+++ client-app/src/App.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="h-screen w-full max-w-4xl mx-auto p-4 pt-12 flex flex-col justify-between">
+  <div class="h-screen w-full max-w-4xl mx-auto p-4 pt-6 flex flex-col justify-between">
     <div class="absolute left-10 top-1 font-bold">WormsAI</div>
-    <ChatsContainer class="flex-1 p-2 overflow-auto" />
-    <ChatBox />
+    <el-tabs v-model="activeName" stretch>
+      <el-tab-pane label="Чат" name="first">
+        <ChatsContainer class="flex-1 p-2 h-[80vh] overflow-scroll" />
+        <ChatBox />
+      </el-tab-pane>
+      <el-tab-pane label="Документ" name="second">
+        <PdfView />
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
 <script setup lang="ts">
 import ChatBox from "./components/ChatBox.vue";
 import ChatsContainer from "./components/ChatsContainer.vue";
+import { ref } from "vue";
+import PdfView from "@/components/PdfView.vue";
+
+const activeName = ref("first");
 </script>
---
+++ client-app/src/components/PdfView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <iframe :src="pdfUrl" width="100%" height="680"></iframe>
+  </div>
+</template>
+
+<script setup lang="ts">
+const pdfUrl = new URL('@/assets/source.pdf', import.meta.url).href;
+</script>
+
+<style scoped></style>
